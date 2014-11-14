53922a894158c626350b2efa10f6df2c1d4034b0
Robert Gilmore <Robert.Warren.Gilmore@gmail.com>
2014-11-14 13:44:36 -0500

    Grabbed stuff from my old Grep project.

---
+++ src/CGrep.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.*;
+
+public class CGrep {
+
+	 private static void solve(Executor e, Collection<Callable<Found>> solvers) throws InterruptedException, ExecutionException {
+	     ExecutorCompletionService<Found> ecs = new ExecutorCompletionService<Found>(e);
+	     for (Callable<Found> s : solvers)
+	         ecs.submit(s);
+	     for (int i = 0; i < solvers.size(); i++) {
+	    	 Found result = ecs.take().get();
+	         if (result != null) {
+	             System.out.println(result.getFileName() +" completed.");
+	             System.out.println(result);
+	         }
+	     }
+	     
+	 }
+	
+    public static void main(String[] args) throws InterruptedException, ExecutionException{
+        String pattern = args[0];
+        String[] fileNames = Arrays.copyOfRange(args, 1, args.length);
+        Collection<Callable<Found>> solvers = new ArrayList<Callable<Found>>();
+        for (String fileName: fileNames) {
+            solvers.add(new GrepCallable(fileName, pattern));
+        }
+        ExecutorService e = Executors.newFixedThreadPool(3);
+        solve(e, solvers);
+        e.shutdown();
+    }
+}
---
+++ src/Found.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Found {
+    private String fileName;
+    private String[] lines;
+
+    public Found(String fileName, String[] lines) {
+        this.fileName = fileName;
+        this.lines = lines;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public List<String> getLines() {
+        return Arrays.asList(lines);
+    }
+    
+    public String toString() {
+    	String string = "";
+    	for(String l : lines){
+    		string = string.concat(l + "\n");
+    	}
+    	return string;
+    }
+}
